f45bda6623220ba5c7006fa71e2daa4e0f247127
İslam Uyanık <90751829+uyanikislam@users.noreply.github.com>
2022-10-15 21:55:36 +0300

    cari

--- models/cariModel.js
+++ models/cariModel.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 
-const {Schema} = mongoose
+const {Schema} = mongoose;
 const cariSchema = new Schema({
     name:{
         type: String,
-        required: true
+        required: true,
+        trim:true,
     },
     description: {
         type: String,
         required: true,
+        trim:true,
     },
 
     amount: {
         type: String,
         required: true,
+        trim:true,
     },
 
     uploadedAt: {
@@ -23,6 +26,6 @@ const cariSchema = new Schema({
 
 });
 
-const Cari = mongoose.model("Cari", cariSchema)
+const Cari = mongoose.model('Cari', cariSchema)
 
 export default Cari;
